b0c026ec1cb469467e350fab924e6ee6b07d4e01
Arthur Guigand <arthur@guigand.com>
2015-03-31 21:16:46 +1300

    add comment form builder

--- src/Clemenger/CommentBundle/Controller/DefaultController.php
+++ src/Clemenger/CommentBundle/Controller/DefaultController.php
@@ -3,11 +3,20 @@
 namespace Clemenger\CommentBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Clemenger\CommentBundle\Entity\Comment;
+use Clemenger\CommentBundle\Form\CommentType;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ClemengerCommentBundle:Default:index.html.twig');
+        $comment = new Comment();
+
+        $form = $this->createForm(new CommentType(), $comment);
+        $form->add('save', 'submit', array('label' => 'Submit'));
+
+        return $this->render('ClemengerCommentBundle:Default:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ src/Clemenger/CommentBundle/Entity/Comment.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Clemenger\CommentBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use \DateTime;
+
+/**
+ * Comment
+ *
+ * @ORM\Table(name="comments")
+ * @ORM\Entity
+ */
+class Comment
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=45)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=45)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="comment", type="text")
+     */
+    private $comment;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    public function __construct(){
+        $this->date = new DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Comment
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Comment
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     * @return Comment
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string 
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
