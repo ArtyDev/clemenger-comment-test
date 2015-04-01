37058ff316a6fcb3f6f411b4e063521252668664
Arthur Guigand <arthur@guigand.com>
2015-04-01 17:16:38 +1300

    allow users to link comment with #{comment_id}

---
+++ src/Clemenger/CommentBundle/Twig/CommentExtension.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arthur
+ * Date: 4/1/15
+ * Time: 4:47 PM
+ */
+
+namespace Clemenger\CommentBundle\Twig;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+
+class CommentExtension extends \Twig_Extension
+{
+    /**
+     * @inheritDoc
+     */
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('parse_comments_references', array($this, 'parseCommentsReferences')),
+        );
+    }
+
+    /**
+     * Parse a single comment to replace comment references by links
+     *
+     * @param $comment
+     * @return string
+     */
+    public function parseCommentsReferences($comment)
+    {
+        return preg_replace("/#(\d+)/", '<a href="/comment/$1">#$1</a>', $comment);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName()
+    {
+        return 'comment_extension';
+    }
+}
