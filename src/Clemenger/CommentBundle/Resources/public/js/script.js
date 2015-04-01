32bdd3b1df6331de57fb13b82c63b848b80a4e59
Arthur Guigand <arthur@guigand.com>
2015-04-01 18:16:06 +1300

    fix bug append text to textarea

--- src/Clemenger/CommentBundle/Resources/public/js/script.js
+++ src/Clemenger/CommentBundle/Resources/public/js/script.js
@@ -4,8 +4,9 @@
 $( document ).ready(function() {
     // References the comment in the textarea on click on the link "ref" for each comment
     $('a.comment-ref').click(function(e){
+        console.log('pass');
         var $commentId = $(this).parent().attr('id').replace('comment-', ''); // get the comment id
-        $('#clemenger_commentbundle_comment_comment').append('#'+$commentId); // append it to the text area
+        $('#clemenger_commentbundle_comment_comment').val($('#clemenger_commentbundle_comment_comment').val()+'#'+$commentId); // append it to the text area
         e.preventDefault();
     });
 });
